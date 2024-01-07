bdeb9448957e918c903a6fdc4c24d16fc4358c3b
matan solom <matan.s.dev@gmail.com>
2024-01-07 23:17:14 +0200

    fc

--- index.js
+++ index.js
@@ -1,13 +1,52 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-require('dotenv').config()
+require('dotenv').config();
+const mongoose=require('mongoose');
+
+
+
+
+mongoose.connect(process.env.MONGO_URI, ()=>{
+  
+}).then(()=> console.log("connected to db"))
+.catch((err)=>{console.error(err);})
+
+
 
 app.use(cors())
+app.use(express.urlencoded({extended:true}))
 app.use(express.static('public'))
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
+
+const userNames=[]
+const ids=[]
+
+app.post("/api/users",(req, res)=>{
+const userName=req.body.username
+const isHere=userNames.indexOf(userName)
+
+  if(isHere < 0 ){
+
+      userNames.push(userName)
+      ids.push(userNames.length)
+
+      return res.json({
+        userName : userName,
+        id:ids[ids.length-1]
+    })
+  }
+    return res.json({
+      userName : userName,
+      id : ids[isHere]
+  })
+})
+
+
+app.post("/api/exe")
  
 
 
