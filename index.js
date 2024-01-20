0af635e3ffa20f40d54f787a271b5d48bff9d6d2
matan solom <matan.s.dev@gmail.com>
2024-01-20 18:40:39 +0200

    Update index.js

--- index.js
+++ index.js
@@ -70,17 +70,6 @@ app.post("/api/users", async (req, res) => {
   ).exec();
   res.json(user)
 });
-// {
-//   username: "fcc_test",
-//   count: 1,
-//   _id: "5fb5853f734231456ccb3b05",
-//   log: [{
-//     description: "test",
-//     duration: 60,
-//     date: "Mon Jan 01 1990",
-//   }]
-
-
 
 
 app.post("/api/users/:_id/exercises", async (req, res) => {
